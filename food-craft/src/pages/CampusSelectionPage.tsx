8983e97d46cf872ee516abba5a7e16e1f94a848a
endidii <thieuandyhuy@gmail.com>
2024-01-03 16:08:08 +0100

    added axios for api calls, CampusSelectionPage.css and CampusSelectionPage.tsx, only show canteens of selected uni missing, currently showing all canteens

--- food-craft/src/pages/CampusSelectionPage.tsx
+++ food-craft/src/pages/CampusSelectionPage.tsx
@@ -1,12 +1,29 @@
-import {ReactElement} from "react";
-import "../styles/GeneralStylesheet.css";
-import "../styles/UniversitySelectionPage.css"
+import React, {ReactElement, useEffect} from "react";
+import "../styles/CampusSelectionPage.css";
 import logo from "../ressources/FoodCraft-Icon-transparent.png";
-import googleMapsSearchIcon from "../ressources/GoogleMaps-search-Icon.png";
-import DropdownBox from "../components/DropdownBox";
 import {useNavigate} from "react-router-dom";
+import axios from "axios";
 
-function WelcomePage(): ReactElement {
+export default function CampusSelectionPage(): ReactElement {
+    interface Address {
+        street: string;
+        city: string;
+        zipcode: string;
+        district: string;
+        geoLocation: {
+            longitude: number;
+            latitude: number;
+        };
+    }
+
+    interface Canteen {
+        name: string;
+        address: Address;
+        id: string;
+    }
+
+    const [canteens, setCanteens] = React.useState<Canteen[]>([]);
+
     const roles = [
         { label: "HTW Berlin", value: 'htw-berlin'}
     ]
@@ -17,6 +34,17 @@ function WelcomePage(): ReactElement {
         navigate('/university-selection');
     };
 
+    useEffect( () => {
+        axios
+            .get("https://mensa.gregorflachs.de/api/v1/canteen", {
+                headers: {
+                    "X-API-KEY": "pzVf9YIu2KOTssG0dsLtU9/G6F6CWhocOK+TjmMkB7RVtCpEDVJ46aZzoe544nHRQlcaF8tSSnOTkuIPhIa22TwSdGzST7JCPMUwMsn0B6C3VwG9W98Y6at5EwfePtfflKmIEFgGc1c1lGI2JZzjPy4LsR4GmkdrJaCTrdYcVksJWMinf6fuzdnpx0i+Yx8ah9eZOKw8/DONX2GXLguKSP+N9/MA7CrdChsNIIbGyJqR/hZXMBOCcbU1c0CxPM64Hd7QTImeAxjkFw6UpGE1xvxBvSYOA5e23ep1f+5DNyazNVt+ofztgYQcn/jcLXCUae674NA8m54U2vQBBBxI6w=="
+                }
+            })
+            .then(response => {
+                setCanteens(response.data);
+            });
+    }, [])
     return (
         <div className="page">
             <header>
@@ -24,10 +52,28 @@ function WelcomePage(): ReactElement {
                 <h1 className="heading">MealCraft</h1>
                 <h2 className="sub-heading">Select Campus</h2>
             </header>
-            <button className="continue-button">Continue</button>
+
+            <div className="canteen-list">
+                {canteens.map((canteen) => (
+                    <button
+                        key={canteen.id}
+                        className="canteen-button"
+                        onClick={() => {
+                            // Navigate to canteen detail page, passing the canteen ID or any other relevant data
+                            navigate(`/canteen-detail/${canteen.id}`);
+                        }}
+                    >
+                        <h3>{canteen.name}</h3>
+                        <p>{`${canteen.address.street}, ${canteen.address.city}, ${canteen.address.zipcode}, ${canteen.address.district}`}</p>
+                    </button>
+                ))}
+            </div>
+
+            <div className="navigation-buttons">
+                <button className="back-button" onClick={navigateToUniversitySelection}>&lt; select university</button>
+                <button className="continue-button">Continue &gt;</button>
+            </div>
 
         </div>
     );
 }
-
-export default WelcomePage;
