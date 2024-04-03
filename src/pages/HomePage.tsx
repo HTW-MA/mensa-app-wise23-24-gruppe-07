78d550f300d77d0ba6ba5c8130fe91138b1866a5
Marvin Petsch <marvinpetsch@mailbox.org>
2024-04-03 19:21:03 +0200

    setup user subscription to push-notifications

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -39,22 +39,19 @@ export default function HomePage(): ReactElement {
       console.error("Error toggling bookmark:", error);
     }
 
-    if (Notification.permission == "granted") {
-      showNotification()
-    } else if (Notification.permission == "denied") {
-      console.log("Notifications denied")
-    } else if (Notification.permission == "default") {
-      Notification.requestPermission().then(function (permission) {
-        if (permission == "granted") {
-          showNotification()
-        } else if (permission == "denied") {
-          console.log("Notifications denied")
-        } else if (permission == "default") {
-          console.log("Notifications can't be send, but can ask for permission again")
-        }
-      });
-    }
+    tryPushNotification();
   };
+
+  const urlBase64ToUint8Array = function(base64String: string) {
+    let padding = "=".repeat((4 - base64String.length % 4) % 4);
+    let base64 = (base64String + padding).replace(/\-/g, "+").replace(/_/g, "/");
+    let rawData = window.atob(base64);
+    let outputArray = new Uint8Array(rawData.length);
+    for (let i = 0; i < rawData.length; ++i) {
+      outputArray[i] = rawData.charCodeAt(i);
+    }
+    return outputArray;
+  }
 
   const showNotification = () => {
     if ("serviceWorker" in navigator) {
@@ -73,6 +70,36 @@ export default function HomePage(): ReactElement {
     });
   }
 
+  const tryPushNotification = () => {
+    let subscribeOptions = {
+      userVisibleOnly: true,
+      applicationServerKey: urlBase64ToUint8Array("BEz1Q_pBcwp0FszWT8xlf3ecUF2IqJA2qaGXR6OYFmWUODcgH5MXu0wZqONEzeAzYlFXS_6gsYsjA057ryiJsAo")
+    }
+
+    navigator.serviceWorker.ready.then(function (registration) {
+      return registration.pushManager.subscribe(subscribeOptions)
+    }).then(function(subscription) {
+      console.log(subscription)
+    });
+
+    /*
+    if (Notification.permission == "granted") {
+      showNotification()
+    } else if (Notification.permission == "denied") {
+      console.log("Notifications denied")
+    } else if (Notification.permission == "default") {
+      Notification.requestPermission().then(function (permission) {
+        if (permission == "granted") {
+          showNotification()
+        } else if (permission == "denied") {
+          console.log("Notifications denied")
+        } else if (permission == "default") {
+          console.log("Notifications can't be send, but can ask for permission again")
+        }
+      });
+    }
+     */
+  }
 
   const mealTypes = [
     { label: "Essen", value: "Essen" },
---
+++ src/push-keys.ts
@@ -0,0 +1,8 @@
+module.exports = {
+    GCMAPIKey: "aMp3eNiib-q9KfnpdZBicyWUlcUv-nmZuTwFHY20-fM",
+    subject: "marvinpetsch@gmail.com",
+    publicKey: "BEz1Q_pBcwp0FszWT8xlf3ecUF2IqJA2qaGXR6OYFmWUODcgH5MXu0wZqONEzeAzYlFXS_6gsYsjA057ryiJsAo",
+    privateKey: "MHx4b83brRmpv5jhIpGe56IlSuIfDM7udSKRC9P2-a4"
+}
+
+export {}
