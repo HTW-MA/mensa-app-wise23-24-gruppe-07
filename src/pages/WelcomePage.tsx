e7f20ef58d26f1a43f706ec1a147e6ce07937997
endidii <thieuandyhuy@gmail.com>
2024-04-04 12:23:37 +0200

    notification

--- src/pages/WelcomePage.tsx
+++ src/pages/WelcomePage.tsx
@@ -7,6 +7,44 @@ import DropdownBox from "../components/DropdownBox";
 import { useNavigate } from "react-router-dom";
 import {checkAndAddCanteens} from "../canteenStore";
 import axios from "axios";
+import { getMessaging, getToken } from "firebase/messaging";
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAu0VR7T-djlZmXJc7TdyCK0io6XnsRrpE",
+    authDomain: "mealcraft-5706a.firebaseapp.com",
+    projectId: "mealcraft-5706a",
+    storageBucket: "mealcraft-5706a.appspot.com",
+    messagingSenderId: "135918648926",
+    appId: "1:135918648926:web:13a49b81f017bca2b07ecf",
+    measurementId: "G-L339LKZKJ8"
+};
+
+const app = initializeApp(firebaseConfig);
+const messaging = getMessaging(app);
+
+const requestPermission = async () => {
+    const permission = await Notification.requestPermission();
+
+    if (permission === "granted") {
+        console.log("Notification permission granted.");
+
+        try {
+            const currentToken = await getToken(messaging, { vapidKey: "BPxGiWSV4KsC-ewbD1GVAG5mghofW68z5K-ampw4Pv3qL4_m-Y50aa7uHK7pEl_GGsSb_YtnsTpBn8xZtxn02R0" });
+            if (currentToken) {
+                console.log("Device token:", currentToken);
+                // Here, you might want to send the token to your server or store it for later use
+                // Since you're operating without a backend, you could store it in the client or ignore if you're only doing broad notifications
+            } else {
+                console.log("No registration token available. Request permission to generate one.");
+            }
+        } catch (err) {
+            console.error("An error occurred while retrieving token. ", err);
+        }
+    } else {
+        console.log("User denied the notification permission.");
+    }
+};
 
 export default function WelcomePage(): ReactElement {
     const roles = [
@@ -47,6 +85,7 @@ export default function WelcomePage(): ReactElement {
         }).catch(error => {
             console.error('Error fetching canteens:', error);
         });
+        requestPermission();
     }, []); // Empty dependency array to ensure it runs only once on component mount
 
     const navigate = useNavigate();
