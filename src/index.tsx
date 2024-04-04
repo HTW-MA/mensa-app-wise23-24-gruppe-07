54d3c157856f1da7f17eeaad9f6d9491a7f8cf22
endidii <thieuandyhuy@gmail.com>
2024-04-04 12:41:45 +0200

    service worker

--- src/index.tsx
+++ src/index.tsx
@@ -6,17 +6,6 @@ import reportWebVitals from './reportWebVitals';
 
 import App from "./App";
 
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/firebase-messaging-sw.js')
-        .then((registration) => {
-            console.log('Service Worker registered with scope:', registration.scope);
-        })
-        .catch((err) => {
-            console.error('Service Worker registration failed:', err);
-        });
-}
-
-
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
